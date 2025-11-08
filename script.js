13e7d79b66e1ffdc3066e7de9a931f5d19b120c4
Vaishhhdeveloper <vaisalymurugan@gmail.com>
2025-11-08 19:50:11 +0530

    commit

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const closePopupBtn = document.getElementById("closePopupBtn");
-    const imagePopup = document.getElementById("imagePopup");
-
-    // This line forces the popup to show as soon as the HTML content is loaded
-    imagePopup.style.display = "flex";
-
-    // Function to close the popup when the user clicks the 'x' button
-    closePopupBtn.addEventListener("click", () => {
-        imagePopup.style.display = "none";
-    });
-
-    // Function to close the popup if the user clicks outside of the content
-    window.addEventListener("click", (event) => {
-        if (event.target === imagePopup) {
-            imagePopup.style.display = "none";
-        }
-    });
-});
- document.addEventListener("DOMContentLoaded", function() {
         const currentPath = window.location.pathname.split('/').pop();
         const navItems = document.querySelectorAll('.nav-item');
 
